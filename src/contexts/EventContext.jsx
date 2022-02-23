ca7c4cd62e35ac6859ce70f598343579ce88b8e0
Ananpiya134 <anan.piyapanee@gmail.com>
2022-02-24 04:40:16 +0700

    found error

--- src/contexts/EventContext.jsx
+++ src/contexts/EventContext.jsx
@@ -5,16 +5,24 @@ const EventContext = createContext();
 
 function EventContextProvider({ children }) {
 	const [allEvent, setAllEvent] = useState([]);
+	const [currentEvent, setCurrentEvent] = useState(null);
+	const [eventIndex, setCurrentIndex] = useState(0);
 
 	useEffect(() => {
 		const fetchEvent = async () => {
 			const res = await axios.get("/events");
 			setAllEvent(res.data);
+			setCurrentEvent(res.data.events[0]);
 		};
 		fetchEvent();
 	}, []);
 
+	useEffect(() => {
+		if (currentEvent !== null) setCurrentEvent(allEvent[eventIndex]);
+	}, [allEvent]);
+
 	console.log(allEvent);
+	console.log(currentEvent);
 
 	return <EventContext.Provider value={{}}>{children}</EventContext.Provider>;
 }
