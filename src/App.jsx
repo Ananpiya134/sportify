fde84d7fc466e01e293ca1feb75ba688aca4c6eb
Ananpiya134 <anan.piyapanee@gmail.com>
2022-03-03 00:53:48 +0700

    breakdown branch

--- src/App.jsx
+++ src/App.jsx
@@ -10,37 +10,37 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
-  const { error, setError } = useContext(ErrorContext);
-  const { message, setMessage } = useContext(ToastContext);
-  useEffect(() => {
-    if (error && error !== "") {
-      toast.error(error);
-      return setError("");
-    }
-  }, [error]);
+	const { error, setError } = useContext(ErrorContext);
+	const { message, setMessage } = useContext(ToastContext);
+	useEffect(() => {
+		if (error && error !== "") {
+			toast.error(error);
+			return setError("");
+		}
+	}, [error]);
 
-  useEffect(() => {
-    if (message && message !== "") {
-      return setMessage("");
-    }
-  }, [message]);
+	useEffect(() => {
+		if (message && message !== "") {
+			return setMessage("");
+		}
+	}, [message]);
 
-  return (
-    <>
-      <ToastContainer
-        position="top-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <RouteConfig />
-    </>
-  );
+	return (
+		<>
+			<ToastContainer
+				position="top-left"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+			<RouteConfig />
+		</>
+	);
 }
 
 export default App;
