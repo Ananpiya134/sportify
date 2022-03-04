c5c7d8d0b5a862fa4ec59ff38f0c2d422748582a
Ananpiya134 <anan.piyapanee@gmail.com>
2022-03-04 17:05:00 +0700

    commit before pulling

--- src/components/Calendar/EventList.jsx
+++ src/components/Calendar/EventList.jsx
@@ -1,32 +1,14 @@
 import React, { useState } from "react";
-import { useContext } from "react";
 import Event from "./event/Event";
 
 function EventList({ events }) {
-  console.log(events);
-  // const [startDate, setStartDate] = useState(new Date());
-  //   console.log(startDate);
-
-  return (
-    <>
-      {/* {events && events.map((item) => <Event />)} */}
-      {/* <div classNameName="event-calendar" style={{ padding: "10px" }}>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          inline
-          monthsShown={1}
-          //   highlightDates={[new Date("2022-01-21")]}
-          highlightDates={startDate}
-        />
-      </div> */}
-      {/* {JSON.stringify(data[0].date)} */}
-
-      {events.map((item) => (
-        <Event key={item.id} data={item} />
-      ))}
-    </>
-  );
+	return (
+		<>
+			{events.map((item) => (
+				<Event key={item.id} data={item} />
+			))}
+		</>
+	);
 }
 
 export default EventList;
